df3281114adf8a6058410e43397f26e488b042a4
Olutunmie <olutunmie@outlook.com>
2022-12-24 17:44:44 +0100

    post router done

--- lib/rest_api/router.ex
+++ lib/rest_api/router.ex
@@ -44,6 +44,30 @@ defmodule RestApi.Router do
     |> put_resp_content_type("application/json")
     |> send_resp(200, posts)
   end
+  
+  post "/post" do
+    case conn.body_params do
+      %{"name" => name, "content" => content} ->
+        case Mongo.insert_one(:mongo, "Posts", %{"name" => name, "content" => content}) do
+          {:ok, user} ->
+            doc = Mongo.find_one(:mongo, "Posts", %{_id: user.interested_id})
+            
+            post = 
+              JSON.normaliseMongoId(doc)
+              |> Jason.encode!()
+              
+            conn 
+            |> put_resp_content_type("application/json")
+            |> send_resp(200, post)
+
+          {:error, _} ->
+            send_resp(conn, 500, "Something went wrong")
+        end
+      
+      _ -> 
+        send_resp(conn, 400, '')
+    end
+  end
 
   # Fallback handler when there was no match
   match _ do
