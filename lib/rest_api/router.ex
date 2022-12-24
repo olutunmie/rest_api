3268fd3812390f071264361c5a6c471b693cf40d
Olutunmie <olutunmie@outlook.com>
2022-12-24 17:48:26 +0100

    basic crubd operation done

--- lib/rest_api/router.ex
+++ lib/rest_api/router.ex
@@ -36,37 +36,99 @@ defmodule RestApi.Router do
   get "/posts" do
     posts =
       Mongo.find(:mongo, "Posts", %{})
-      |>Enum.map(&JSON.normaliseMongoId/1)
-      |>Enum.to_list()
-      |>Jason.encode!()
+      |> Enum.map(&JSON.normaliseMongoId/1)
+      |> Enum.to_list()
+      |> Jason.encode!()
 
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, posts)
   end
-  
+
   post "/post" do
     case conn.body_params do
       %{"name" => name, "content" => content} ->
         case Mongo.insert_one(:mongo, "Posts", %{"name" => name, "content" => content}) do
           {:ok, user} ->
             doc = Mongo.find_one(:mongo, "Posts", %{_id: user.interested_id})
-            
-            post = 
+
+            post =
               JSON.normaliseMongoId(doc)
               |> Jason.encode!()
-              
-            conn 
+
+            conn
             |> put_resp_content_type("application/json")
             |> send_resp(200, post)
 
           {:error, _} ->
             send_resp(conn, 500, "Something went wrong")
         end
-      
-      _ -> 
+
+      _ ->
         send_resp(conn, 400, '')
     end
+  end
+
+  get "/post/:id" do
+    doc = Mongo.find_one(:mongo, "Posts", %{_id: BSON.ObjectId.decode!(id)})
+
+    case doc do
+      nil ->
+        send_resp(conn, 404, "Not Found")
+
+      %{} ->
+        post =
+          JSON.normaliseMongoId(doc)
+          |> Jason.encode!()
+
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(200, post)
+
+      {:error, _} ->
+        send_resp(conn, 500, "Something went wrong")
+    end
+  end
+
+  put "post/:id" do
+    case Mongo.find_one_and_update(
+           :mongo,
+           "Posts",
+           %{_id: BSON.ObjectId.decode!(id)},
+           %{
+             "$set":
+               conn.body_params
+               |> Map.take(["name", "content"])
+               |> Enum.into(%{}, fn {key, value} -> {"#{key}", value} end)
+           },
+           return_document: :after
+         ) do
+      {:ok, doc} ->
+        case doc do
+          nil ->
+            send_resp(conn, 404, "Not Found")
+
+          _ ->
+            post =
+              JSON.normaliseMongoId(doc)
+              |> Jason.encode!()
+
+            conn
+            |> put_resp_content_type("application/json")
+            |> send_resp(200, post)
+        end
+
+      {:error, _} ->
+        send_resp(conn, 500, "Something went wrong")
+    end
+  end
+
+  delete "post/:id" do
+    Mongo.delete_one!(:mongo, "Posts", %{_id: BSON.ObjectId.decode!(id)})
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Jason.encode!(%{id: id}))
   end
 
   # Fallback handler when there was no match
